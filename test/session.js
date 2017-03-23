1276bb82193e12d6f63c75e31014731adf507d31
Rob Huehn <rhuehn@gmail.com>
2017-03-22 20:43:13 -0700

    session test works

--- test/session.js
+++ test/session.js
@@ -1,10 +1,6 @@
 'use strict'
 const {createSession} = require('@theatersoft/server')
 
-createSession('name', 'ip', 'ua')
-    .then(r => {
-        console.log('createSession returned', r)
-    }, e => {
-        console.log('createSession rejected', e)
-    })
+const r = createSession('name', 'ip', 'ua')
+console.log('createSession returned', r)
 
