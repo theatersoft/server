b9175d3a1f761e3a4c6a7204a61252efa792dbfd
Rob Huehn <rhuehn@gmail.com>
2017-03-22 19:30:28 -0700

    clean up config test

--- test/config.js
+++ test/config.js
@@ -1,16 +1,6 @@
 'use strict'
-//const
-//    {rpc} = require('./rpc')
-//
-//rpc('Config.Get')
-//    .then(r => {
-//        console.log('Config.Get returned', r)
-//    }, e => {
-//        console.log('Config.Get rejected', e)
-//    })
-
 const
-    {default: bus, proxy} = require('@theatersoft/bus'),
+    {bus, proxy} = require('@theatersoft/bus'),
     Config = proxy('Config')
 
 bus.start()
