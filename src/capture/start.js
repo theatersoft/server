bdb50ba2dd2e97ca4f2f2277d61782eb7712ff4d
Rob Huehn <rhuehn@gmail.com>
2017-12-15 19:48:40 -0800

    convert capture start to lsm

--- src/capture/start.js
+++ src/capture/start.js
@@ -1,12 +1,17 @@
 'use strict'
 const
     {bus} = require('@theatersoft/bus'),
+    {LocalServiceManager} = require('@theatersoft/server/localServiceManager'),
     options = {
         module: '@theatersoft/server/capture/capture',
-        export: 'Capture'
+        export: 'Capture',
+        name: 'capture'
     },
     service = new (require(options.module)[options.export])()
 
-bus.start().then(() => service.start())
+bus.start().then(() => new LocalServiceManager(
+    options.name,
+    {[options.name]: {options}}
+))
 process.on('SIGINT', () => process.exit())
 process.on('exit', () => service.stop())
