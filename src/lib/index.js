cc713363781cb663d82d8c591e8742bbb8bfd44e
Rob Huehn <rhuehn@gmail.com>
2017-12-16 20:06:34 -0800

    set time and tag in startLocalService

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,13 +1,14 @@
-import {bus, debug} from '@theatersoft/bus'
 import {LocalServiceManager} from  '../localServiceManager'
-
 export {LocalServiceManager}
 
+import {bus, setTime, setTag} from '@theatersoft/bus'
+
 export function startLocalService (options) {
+    const {name} = options
+    setTime(true)
+    setTag(name)
     bus.start().then(() => {
-        const
-            {name} = options,
-            lsm = new LocalServiceManager(name, {[name]: {options}})
+        const lsm = new LocalServiceManager(name, {[name]: {options}})
         process
             .on('SIGINT', () => process.exit())
             .on('exit', () => lsm.stopService(name))
