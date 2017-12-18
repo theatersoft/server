7c2c23344f38fce18a5f3d42e5b5228ab1aa8e97
Rob Huehn <rhuehn@gmail.com>
2017-12-18 08:25:03 -0800

    add startLocalService service config lookup mode

--- src/lib/index.js
+++ src/lib/index.js
@@ -3,14 +3,18 @@ export {LocalServiceManager}
 
 import {bus, setTime, setTag} from '@theatersoft/bus'
 
-export function startLocalService (options) {
-    const {name} = options
-    setTime(true)
-    setTag(name)
-    bus.start().then(() => {
-        const lsm = new LocalServiceManager(name, {[name]: {options}})
-        process
-            .on('SIGINT', () => process.exit())
-            .on('exit', () => lsm.stopService(name))
-    })
+export function startLocalService (service) {
+    bus.start()
+        .then(async () => {
+            const options = service instanceof Object
+                ? service
+                : await bus.proxy('Config').getServiceOptions(service)
+            const {name} = options
+            setTime(true)
+            setTag(name)
+            const lsm = new LocalServiceManager(name, {[name]: {options}})
+            process
+                .on('SIGINT', () => process.exit())
+                .on('exit', () => lsm.stopService(name))
+        })
 }
