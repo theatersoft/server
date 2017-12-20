4d7014f8e6db2cf25a82389ca9074d34c35ebb7c
Rob Huehn <rhuehn@gmail.com>
2017-12-20 10:41:12 -0800

    fix capture module path

--- src/capture/start.js
+++ src/capture/start.js
@@ -1,6 +1,6 @@
 'use strict'
 require('@theatersoft/server/lib').startLocalService({
-    module: '@theatersoft/server/capture/capture',
+    module: '@theatersoft/server/capture',
     export: 'Capture',
     name: `capture.${require('os').hostname()}`
 })
