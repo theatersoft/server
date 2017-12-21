b0d8dd10d46534f1eb6c7cc1c116e3107f78bb56
Rob Huehn <rhuehn@gmail.com>
2017-12-21 07:34:06 -0800

    rm redundant log tag

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,17 +1,16 @@
 import {LocalServiceManager} from  '../localServiceManager'
 export {LocalServiceManager}
 
-import {bus, setTime, setTag} from '@theatersoft/bus'
+import {bus, setTime} from '@theatersoft/bus'
 
 export function startLocalService (service, enabled = true) {
+    setTime(true)
     bus.start()
         .then(async () => {
             const options = service instanceof Object
                 ? service
                 : await bus.proxy('Config').getServiceOptions(service)
             const {name} = options
-            setTime(true)
-            setTag(name)
             options.enabled = enabled
             const lsm = new LocalServiceManager(name, {[name]: {options}})
             process
