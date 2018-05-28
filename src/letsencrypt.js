1f1ccb47765b5129f99528ea1644138ca5b30772
Rob Huehn <rhuehn@gmail.com>
2018-05-27 19:17:19 -0700

    upgrade to Let's Encrypt ACME v2 (draft 11)

--- src/letsencrypt.js
+++ src/letsencrypt.js
@@ -1,53 +1,49 @@
 import {log, error} from '@theatersoft/bus'
 import {THEATERSOFT_CONFIG_HOME} from './config'
 
-const
-    LE = require('greenlock'),
-    store = require('le-store-certbot').create({
-        configDir: `${THEATERSOFT_CONFIG_HOME}/letsencrypt/etc`,
-        debug: false
-    }),
-    challenge = require('le-challenge-fs').create({
-        webrootPath: `${THEATERSOFT_CONFIG_HOME}/letsencrypt/var/`,
-        debug: false
-    })
+export function createServer ({app, port, domain, email}) {
+    const
+        store = require('le-store-certbot').create({
+            configDir: `${THEATERSOFT_CONFIG_HOME}/acme/etc`,
+            webrootPath: `/tmp/acme-challenges/`
+        }),
+        challenge = require('le-challenge-fs').create({
+            webrootPath: `/tmp/acme-challenges/`
+        }),
+        approveDomains = (options, certs, cb) => {
+            // This is where you check your database and associated
+            // email addresses with domains and agreements and such
 
-let le
-export function createServer ({app, port, domain, email, production}) {
-    function approveDomains (options, certs, cb) {
-        // The domains being approved for the first time are listed in opts.domains
-        // Certs being renewed are listed in certs.altnames
-//debugger
-        if (certs)
-            options.domains = certs.altnames
-        else
-            Object.assign(options, {email, agreeTos: true, domain, domains: [domain]})
+            // Opt-in to submit stats and get important updates
+            options.communityMember = false
 
-        cb(null, {options, certs})
-    }
+            // If you wish to replace the default challenge plugin, you may do so here
+            options.challenges = {'http-01': challenge}
 
-    le = LE.create({
-        app,
-        approveDomains,
-        email,
-        server: production ? LE.productionServerUrl : LE.stagingServerUrl,
-        store,                                                           // handles saving of config, accounts, and certificates
-        challenges: {'http-01': challenge},                  // handles /.well-known/acme-challege keys and tokens
-        challengeType: 'http-01',                               // default to this challenge type
-        agreeTos: true,
-        // sni: require('le-sni-auto').create({}),                // handles sni callback
-        debug: false
-        //log
-    })
+            // The domains being approved for the first time are listed in opts.domains
+            // Certs being renewed are listed in certs.altnames
+            if (certs)
+                options.domains = certs.altnames
+            else
+                Object.assign(options, {email, agreeTos: true, domains: [domain]})
+
+            cb(null, {options, certs})
+        },
+        greenlock = require('greenlock').create({
+            version: 'draft-11',
+            server: 'https://acme-v02.api.letsencrypt.org/directory',
+            //server: 'https://acme-staging-v02.api.letsencrypt.org/directory',
+            approveDomains,
+            store
+        })
 
     // handles acme-challenge and redirects to https
-    require('http').createServer(le.middleware(require('redirect-https')()))
+    require('http').createServer(greenlock.middleware(require('redirect-https')()))
         .listen(80, function () {
             log("Listening for ACME http-01 challenges on", this.address())
         })
 
-    // handles your app
-    return require('https').createServer(le.httpsOptions, le.middleware(app))
+    return require('https').createServer(greenlock.tlsOptions, greenlock.middleware(app))
         .listen(port, function () {
             log("Listening for ACME tls-sni-01 challenges and serve app on", this.address())
         })
